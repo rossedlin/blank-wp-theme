4f16aba48bd7113f590f6c6a7463d0bd2af7f0c8
Ross Edlin <rossedlin@gmail.com>
2017-10-01 19:23:32 +0700

    add - api extra fields

--- functions.php
+++ functions.php
@@ -9,3 +9,30 @@
  */
 
 add_theme_support('post-thumbnails');
+
+function ag_filter_post_json($response, $post, $context)
+{
+	/**
+	 * Author
+	 */
+	$response->data['author_meta'] = [
+		'id'           => get_the_author_meta('id', $post->author),
+		'display_name' => get_the_author_meta('display_name', $post->author),
+	];
+
+	/**
+	 * Tags
+	 */
+	$tags                        = wp_get_post_tags($post->ID);
+	$response->data['tag_names'] = [];
+
+	foreach ($tags as $tag)
+	{
+		$response->data['tag_names'][] = $tag->name;
+		$response->data['tag_slugs'][] = $tag->slug;
+	}
+
+	return $response;
+}
+
+add_filter('rest_prepare_post', 'ag_filter_post_json', 10, 3);
